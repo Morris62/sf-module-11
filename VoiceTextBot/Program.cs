b67b5c653a3f991d074bfe4392014ca118b8bbd6
a.chaturov <chaturovai@gmail.com>
2025-03-08 00:15:08 +0300

    unit 11.2 VoiceTextBot

---
+++ VoiceTextBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace VoiceTextBot;
+
+internal class Program
+{
+    private static async Task Main()
+    {
+        //Console.OutputEncoding = Encoding.Unicode;
+
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) => ConfigureServices(services))
+            .UseConsoleLifetime()
+            .Build();
+        
+        Console.WriteLine("Сервис запущен");
+        await host.RunAsync();
+        Console.WriteLine("Сервис остановлен");
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<ITelegramBotClient>(provider =>
+            new TelegramBotClient(""));
+        services.AddHostedService<Bot>();
+    }
+}
